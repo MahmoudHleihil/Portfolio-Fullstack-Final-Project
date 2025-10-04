3ee3cf3ecfc7e98059ed81b881919fc47d6a8862
MahmoudHleihil <mahmodhl0119@gmail.com>
2025-10-04 14:53:36 +0300

    update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
   return (
     <footer className="bg-dark text-white text-center py-3 mt-4">
-      <p>&copy; {new Date().getFullYear()} Your Name. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Mahmoud Hleihil. All rights reserved.</p>
       <p>
         <Link to="/about">About</Link> | 
         <Link to="/contact">Contact</Link>
